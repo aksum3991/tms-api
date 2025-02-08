253cb9c12817e45a9b6b844af22367bf0b9e7189
unknown <mebrhit765@gmail.com>
2025-02-08 09:53:04 +0300

    Approve user list and pagination for User registration request list, approved user list & users status history list added

--- tms_backend/settings.py
+++ tms_backend/settings.py
@@ -88,10 +88,13 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,  
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=2),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
--- tms_backend/urls.py
+++ tms_backend/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from auth_app.views import  AdminNotificationsView, DeactivateUserView, DepartmentViewSet, ReactivateUserView, UserDetailView, UserRegistrationView, AdminApprovalView, UserResubmissionView, UserStatusHistoryViewSet
+from auth_app.views import  ApprovedUsersView, DeactivateUserView, DepartmentViewSet,  ReactivateUserView, UserDetailView, UserRegistrationView, AdminApprovalView, UserResubmissionView, UserStatusHistoryViewSet
 
 router = DefaultRouter()
 router.register(r'status-history', UserStatusHistoryViewSet, basename='status-history')
@@ -16,10 +16,10 @@ urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('approve/<int:user_id>/', AdminApprovalView.as_view(), name='approve'),
     path('users/', AdminApprovalView.as_view(), name='users'),
-    path('user/<int:user_id>/', UserDetailView.as_view(), name='user-detail'),
+    path('users/<int:user_id>/', UserDetailView.as_view(), name='user-detail'),
     path("resubmit/<int:user_id>/", UserResubmissionView.as_view(), name="resubmit"),
     path("activate/<int:user_id>/",ReactivateUserView.as_view(),name="activate"),
     path("deactivate/<int:user_id>/",DeactivateUserView.as_view(),name="activate"),
     path("update-role/<int:user_id>/", AdminApprovalView.as_view(), name="update-role"),
-    path("admin/notifications/",AdminNotificationsView.as_view(),name="admin-notification"),
+    path('approved-users/', ApprovedUsersView.as_view(), name='approved-users'),
 ]
